db2acc265902b10f83127d6041317414d96ce573
孙小涛 <sunxiaotao@admaster.com.cn>
2019-07-30 13:39:09 +0800

    ci: contextApi-hooks demo

--- src/store/combineReducer.ts
+++ src/store/combineReducer.ts
@@ -5,10 +5,25 @@
 import {Reducer} from 'react';
 import {RootState, RootActions} from './interface';
 
-export function combineReducer(
+export function combineReducer1(
   reducers: Record<RootActions['key'], any>
 ): Reducer<RootState, RootActions> {
   return (state, action): RootState => {
+    if (!!reducers[action.key]) {
+      return {
+        ...state,
+        [action.key]: reducers[action.key](state[action.key], action),
+      };
+    }
+
+    return state;
+  };
+}
+
+export function combineReducer<S, A extends {key: keyof S}>(
+  reducers: Record<A['key'], any>
+): Reducer<S, A> {
+  return (state, action): S => {
     if (!!reducers[action.key]) {
       return {
         ...state,
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -4,8 +4,9 @@
 import {combineReducer} from './combineReducer';
 import teamReducer from './teamReducer';
 import counterReducer from './counterReducer';
+import {RootState, RootActions} from './interface';
 
-export const rootReducer = combineReducer({
+export const rootReducer = combineReducer<RootState, RootActions>({
   team: teamReducer,
   counter: counterReducer,
 });
